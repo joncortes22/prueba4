8eca5c12c97d1ea3224983c5445d5d66bd5519eb
JonathanCortes <joncortes2212@gmail.com>
2023-02-05 13:13:16 -0600

    send

--- pruebaJeff.py
+++ pruebaJeff.py
@@ -1,4 +1,10 @@
-print("My name is Jeff")
-num = 5
-print(num)
-print()
+"""
+for x in range(4):
+    num1 = int(input("Ingrese el primer valor: "))
+    num2 = int(input("Ingrese el segundo valor: "))
+    print(x)
+    if num1>num2:
+        print(f"El segundo valor es mayor ({num1})")
+    else:
+        print(f"El segundo valor es mayor ({num2})")
+"""
