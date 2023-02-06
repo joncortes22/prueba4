63bd8e940164dea336d9aaa9639ef825856d7623
JonathanCortes <joncortes2212@gmail.com>
2023-02-05 19:23:28 -0600

    enviar

--- pruebaJeff.py
+++ pruebaJeff.py
@@ -8,3 +8,20 @@ for x in range(4):
     else:
         print(f"El segundo valor es mayor ({num2})")
 """
+
+orden = []
+orden.append(int(input("Ingrese el primer valor: ")))
+orden.append(int(input("Ingrese el segundo valor: ")))
+orden.append(int(input("Ingrese el tercer valor: ")))
+mayor = orden[0]
+
+for x in range(len(orden)): #hacer prueba con doble for, como lo dijo max
+    for i in range(len(orden)):
+        if orden[x]>orden[i]:
+            aux = orden[x]
+            orden[x] = orden[i]
+            orden[i] = aux
+        
+
+for x in orden:
+    print(x)
